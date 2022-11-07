7acf0c9e669e5756c4ab2bc94fdfcf62d8268383
Sean Gabriel Bayron <scbayron@up.edu.ph>
2022-11-07 23:53:31 +0800

    compose page (buttons not yet done)

--- src/pages/Compose/index.tsx
+++ src/pages/Compose/index.tsx
@@ -1,10 +1,46 @@
 import styles from './Compose.module.scss'; 
 import Header from "../../components/Header/Header";
 import Navbar from "../../components//Navbar/Navbar";
+
+import batchIcon from "../../assets/img/group.svg"
+
 function Compose() {
+    function autoAdjust(){
+        let textInput = document.getElementById('inputText')
+        textInput.style.height = "auto"
+        textInput.style.height = (textInput.scrollHeight)+"px"
+    }
+
     return ( 
-        <div id="compose">
+        <div id={styles.compose}>
             <Header />
+
+            <div className={styles.title}>
+                <h1>Send a Message</h1>
+            </div>
+
+            <div className={styles.mssg_box}>
+                    <form>
+                        <textarea type="text"
+                        id='inputText'
+                        placeholder='Message'
+                        className={styles.textBox}
+                        onKeyUp={autoAdjust}
+                        onKeyDown={autoAdjust}
+                        maxLength="160"
+                        />
+
+                        <div className={styles.submit_buttons}>
+                            <button className={styles.submit_single}>Single SMS</button>
+                            <button className={styles.submit_batch}>
+                                <img src={batchIcon} alt="" />
+                                Batch SMS
+                            </button>
+                        </div>
+                    </form>
+            </div>
+
+
             <Navbar />            
         </div>
      );
