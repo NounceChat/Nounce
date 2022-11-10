f2649ad42e901dc2da75572dee93d8b1a28b5b88
Sean Gabriel Bayron <scbayron@up.edu.ph>
2022-11-10 17:47:09 +0800

    Fixed responsiveness and Navbar Icon highlight

--- src/components/ChatHeader/index.tsx
+++ src/components/ChatHeader/index.tsx
@@ -3,16 +3,20 @@ import Grogu from "../../assets/img/Grogu.webp"
 import {useNavigate} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faChevronLeft} from "@fortawesome/free-solid-svg-icons";
+import { useParams } from "react-router-dom";
+import { MyParams } from '../../pages/Chat';
 
 function ChatHeader({chatMate}:any) {
     const navigate = useNavigate();
 
-    // const avatar = `https://avatars.dicebear.com/api/identicon/${chat.id}.svg`
+    const { id } = useParams<keyof MyParams>() as MyParams;
+
+    const avatar = `https://avatars.dicebear.com/api/identicon/${id}.svg`
 
     return ( 
         <div id={styles.chatHeader}>
             <div className={styles.chatInfo}>
-                <img src={Grogu} title='avatar' /> 
+                <img src={avatar} title='avatar' /> 
                 <p>{chatMate}</p>
             <button onClick={() => navigate("/")}>
                 <FontAwesomeIcon icon={faChevronLeft} color="white"/>
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,17 +3,25 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHouse, faGear, faPenToSquare } from "@fortawesome/free-solid-svg-icons"
 import {useNavigate} from "react-router-dom";
 const Navbar = () => {
+  
   const navigate = useNavigate();
+  
+  var iconColors:any = {
+      homeColor: (location.pathname == "/"? 'white' : 'gray'),
+      composeColor: (location.pathname == "/compose"? 'white' :'gray'),
+      settingsColor: (location.pathname == "/settings"? 'white' : 'gray')
+  }
   return (
+
     <nav id={styles.navbar}>
-      <a onClick={()=>{navigate("/")}}>
-        <FontAwesomeIcon icon={faHouse} />
+      <a onClick={()=>{navigate("/"); }}>
+        <FontAwesomeIcon icon={faHouse} color={iconColors.homeColor}/>
       </a>
       <a onClick={()=>{navigate("/compose")}}>
-      <FontAwesomeIcon icon={faPenToSquare} />
+      <FontAwesomeIcon icon={faPenToSquare} color={iconColors.composeColor}/>
       </a>
       <a onClick={()=>{navigate("/settings")}}>
-        <FontAwesomeIcon icon={faGear} />
+        <FontAwesomeIcon icon={faGear} color={iconColors.settingsColor}/>
       </a>
     </nav>
   )
