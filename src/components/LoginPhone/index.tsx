65b7c75a64c084678c4612d6040140832ab2d71d
ArJSarmiento <rneljan@gmail.com>
2023-01-19 21:51:15 +0800

    Iphone Model  Rotation Implementation

--- src/components/LoginPhone/index.tsx
+++ src/components/LoginPhone/index.tsx
@@ -1,25 +1,43 @@
-import { FunctionComponent, Suspense } from 'react';
+// import LoginImg from "../../assets/img/LogInDesk.svg";
+import { FunctionComponent, Suspense } from "react";
 import styles from "./LoginPhone.module.scss";
-import LoginImg from "../../assets/img/LogInDesk.svg";
-import Box from "../../components/Three/Box";
 import AnimatedSphere from "../../components/Three/AnimatedSphere";
+import { OrbitControls } from "@react-three/drei";
+import { Model as Iphone } from "../../components/Three/Iphone";
 import { Canvas } from "@react-three/fiber";
 
-interface LoginPhoneProps { }
- 
+interface LoginPhoneProps {}
+
 const LoginPhone: FunctionComponent<LoginPhoneProps> = () => {
-    return ( 
-    <div className={styles.loginImg}>
-      {/* <img src={LoginImg} alt="loginImg" className={styles.loginImg} /> */}
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[-2, 5, 2]} />
-        <Suspense fallback={null}>
-          <AnimatedSphere></AnimatedSphere>
-        </Suspense>
-      </Canvas>
-    </div>
-   );
-}
- 
+  return (
+    <>
+      <div className={styles.loginImg}>
+        {/* <img src={LoginImg} alt="loginImg" className={styles.loginImg} /> */}
+        <Canvas>
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} />
+          <Suspense fallback={null}>
+            <AnimatedSphere></AnimatedSphere>
+          </Suspense>
+        </Canvas>
+      </div>
+      <div className={styles.phoneModel}>
+        <Canvas>
+          <OrbitControls
+            enableZoom={false}
+            enablePan={false}
+            autoRotate={true}
+            autoRotateSpeed={5}
+          />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} />
+          <Suspense fallback={null}>
+            <Iphone></Iphone>
+          </Suspense>
+        </Canvas>
+      </div>
+    </>
+  );
+};
+
 export default LoginPhone;
--- src/components/Three/AnimatedSphere.tsx
+++ src/components/Three/AnimatedSphere.tsx
@@ -1,13 +1,12 @@
-import React from "react";
 import { MeshDistortMaterial, Sphere } from "@react-three/drei";
 
 export default function AnimatedSphere() {
   return (
-    <Sphere visible args={[1, 100, 200]} scale={2}>
+    <Sphere visible args={[1, 100, 200]} scale={2.5}>
       <MeshDistortMaterial
-        color="#8352FD"
+        color= "#bb01f3"
         attach="material"
-        distort={0.3}
+        distort={0.4}
         speed={1.5}
         roughness={0}
       />
