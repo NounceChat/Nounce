0804f08d2117284a47053745c4b90fe4495d6619
ArJSarmiento <rneljan@gmail.com>
2023-01-20 12:11:06 +0800

    PWA integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,5 +8,35 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 3000,
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg,json,gltf}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.png'],
+      manifest: {
+        name: 'Nounce',
+        short_name: 'Nounce',
+        description: 'Nounce is a free chat app that sends messages based on your geographic proximity.',
+        theme_color: '#2A2A2A',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-256x256.png',
+            sizes: '256x256',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
